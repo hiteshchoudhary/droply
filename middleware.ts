46bd899f4d49a7514c1f4f8b27da0f88e646a54d
Shobhini <130296359+Shobhini@users.noreply.github.com>
2025-05-24 08:05:10 +0530

    Fix: correct parameter in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -12,12 +12,12 @@ export default clerkMiddleware(async (auth, request) => {
   const userId = (await user).userId;
   const url = new URL(request.url);
 
-  if (userId && isPublicRoute(request) && url.pathname !== "/") {
+  if (userId && isPublicRoute(request.nextUrl.pathname) && url.pathname !== "/") {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   // Protect non-public routes
-  if (!isPublicRoute(request)) {
+  if (!isPublicRoute(request.nextUrl.pathname)) {
     await auth.protect();
   }
 });
